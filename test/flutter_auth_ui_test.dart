9b2fc84636dd81f631b1cd08bc729a5383f778d4
Koji Wakamiya <koji.wakamiya@gmail.com>
2020-01-14 09:46:10 +0900

    minor update

--- test/flutter_auth_ui_test.dart
+++ test/flutter_auth_ui_test.dart
@@ -1,15 +1,29 @@
+import 'package:firebase_auth_platform_interface/firebase_auth_platform_interface.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_auth_ui/flutter_auth_ui.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('flutter_auth_ui');
 
   TestWidgetsFlutterBinding.ensureInitialized();
 
+  final mockUser = PlatformUser(
+      providerId: "test",
+      uid: "001",
+      displayName: "test",
+      photoUrl: null,
+      email: null,
+      phoneNumber: null,
+      creationTimestamp: 12345,
+      lastSignInTimestamp: 12345,
+      isAnonymous: false,
+      isEmailVerified: false,
+      providerData: Iterable.empty());
+
   setUp(() {
     channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
+      return mockUser;
     });
   });
 
@@ -17,7 +31,7 @@ void main() {
     channel.setMockMethodCallHandler(null);
   });
 
-  test('getPlatformVersion', () async {
-    expect(await FlutterAuthUi.platformVersion, '42');
+  test('startUi', () async {
+    expect(await FlutterAuthUi.startUi(), mockUser);
   });
 }
