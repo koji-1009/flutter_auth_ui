5af40223f839e323de97d4c79bb4be0a831fe925
Koji Wakamiya <koji.wakamiya@gmail.com>
2024-11-15 08:12:03 +0900

    chore: Update example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_auth_ui/flutter_auth_ui.dart';
@@ -22,60 +23,98 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.light().copyWith(
         materialTapTargetSize: MaterialTapTargetSize.padded,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              ElevatedButton(
-                child: const Text("start ui"),
-                onPressed: () async {
-                  final providers = [
-                    AuthUiProvider.anonymous,
-                    AuthUiProvider.email,
-                    AuthUiProvider.phone,
-                    AuthUiProvider.apple,
-                    AuthUiProvider.github,
-                    AuthUiProvider.google,
-                    AuthUiProvider.microsoft,
-                    AuthUiProvider.yahoo,
-                  ];
+      home: const MyPage(),
+    );
+  }
+}
 
-                  final result = await FlutterAuthUi.startUi(
-                    items: providers,
-                    tosAndPrivacyPolicy: const TosAndPrivacyPolicy(
-                      tosUrl: "https://www.google.com",
-                      privacyPolicyUrl: "https://www.google.com",
+class MyPage extends StatelessWidget {
+  const MyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Plugin example app'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            FilledButton(
+              onPressed: () async {
+                final messenger = ScaffoldMessenger.of(context);
+
+                final providers = [
+                  AuthUiProvider.anonymous,
+                  AuthUiProvider.email,
+                  AuthUiProvider.phone,
+                  AuthUiProvider.apple,
+                  AuthUiProvider.github,
+                  AuthUiProvider.google,
+                  AuthUiProvider.microsoft,
+                  AuthUiProvider.yahoo,
+                ];
+
+                final result = await FlutterAuthUi.startUi(
+                  items: providers,
+                  tosAndPrivacyPolicy: const TosAndPrivacyPolicy(
+                    tosUrl: "https://www.google.com",
+                    privacyPolicyUrl: "https://www.google.com",
+                  ),
+                  androidOption: const AndroidOption(
+                    enableSmartLock: false, // default true
+                    showLogo: true, // default false
+                    overrideTheme: true, // default false
+                  ),
+                  emailAuthOption: const EmailAuthOption(
+                    requireDisplayName: true,
+                    // default true
+                    enableMailLink: false,
+                    // default false
+                    handleURL: '',
+                    androidPackageName: '',
+                    androidMinimumVersion: '',
+                  ),
+                );
+
+                messenger.showSnackBar(
+                  SnackBar(
+                    content: Text(
+                      result ? 'Success sign-in' : 'Failed sign-in',
                     ),
-                    androidOption: const AndroidOption(
-                      enableSmartLock: false, // default true
-                      showLogo: true, // default false
-                      overrideTheme: true, // default false
+                  ),
+                );
+              },
+              child: const Text('Sign in'),
+            ),
+            FilledButton.tonal(
+              onPressed: () async {
+                final messenger = ScaffoldMessenger.of(context);
+                await FlutterAuthUi.signOut();
+                messenger.showSnackBar(
+                  const SnackBar(
+                    content: Text('Success sign-out'),
+                  ),
+                );
+              },
+              child: const Text('Sign out'),
+            ),
+            OutlinedButton(
+              onPressed: () async {
+                final user = FirebaseAuth.instance.currentUser;
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(
+                      user == null
+                          ? 'No user is signed in'
+                          : '${user.uid} is signed in',
                     ),
-                    emailAuthOption: const EmailAuthOption(
-                      requireDisplayName: true,
-                      // default true
-                      enableMailLink: false,
-                      // default false
-                      handleURL: '',
-                      androidPackageName: '',
-                      androidMinimumVersion: '',
-                    ),
-                  );
-                  debugPrint(result.toString());
-                },
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  await FlutterAuthUi.signOut();
-                  debugPrint('Signed out !');
-                },
-                child: const Text('sign out'),
-              ),
-            ],
-          ),
+                  ),
+                );
+              },
+              child: const Text('Check sign-in status'),
+            ),
+          ],
         ),
       ),
     );
